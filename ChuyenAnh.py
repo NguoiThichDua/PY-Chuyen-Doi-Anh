f02b8c8b0be8703eb1b47466aec44a96e4bbc57a
Sơn Nguyễn <nbhs.it.98@gmail.com>
2019-09-27 14:36:17 +0700

    SonNguyen

--- ChuyenAnh.py
+++ ChuyenAnh.py
@@ -2,7 +2,7 @@
 import cv2
 
 # ham chuyen anh nhi phan tu anh xam
-def convert_to_binary(img_grayscale, thresh=100):
+def convert_to_binary(img_grayscale, thresh):
     # threshold(anh_xam, nguong_anh, maxvalue, type)
     thresh, img_binary = cv2.threshold(img_grayscale, thresh, maxval=255, type=cv2.THRESH_BINARY)
     return img_binary
@@ -16,12 +16,12 @@ if __name__ == "__main__":
     cv2.imwrite('AnhXam_Son.jpg', img_grayscale)
     print('Anh xam da duoc luu')
 
-    # luu anh quan pho
-    hsv_img = cv2.cvtColor(cv2.imread('Son.jpg',1), cv2.COLOR_BGR2HSV)
-    cv2.imwrite('AnhQuangPho_Son.jpg', hsv_img)
-    print('Anh quang pho da duoc luu')
+    # luu anh quang pho
+    #hsv_img = cv2.cvtColor(cv2.imread('Son.jpg',1), cv2.COLOR_BGR2HSV)
+    #cv2.imwrite('AnhQuangPho_Son.jpg', hsv_img)
+    #print('Anh quang pho da duoc luu')
 
     # luu anh nhi phan tu anh xam
-    img_binary = convert_to_binary(img_grayscale, thresh=100)
+    img_binary = convert_to_binary(img_grayscale, 100)
     cv2.imwrite('AnhNhiPhan_Son.jpg', img_binary)
     print('Anh nhi phan da duoc luu')
